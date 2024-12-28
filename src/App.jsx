da370187c60c6f0b2635e354757abebe89ed384e
MukterHossain <MukterHossain>
2024-12-28 22:13:56 +0600

    green fashion project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <>
+      <button className="btn btn-info text-3xl">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </>
+  );
+}
+
+export default App;
